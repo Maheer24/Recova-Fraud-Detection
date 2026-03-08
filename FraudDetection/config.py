6713ee046fa53814bb5d53c4251be0cf6837ad01
Maheer24 <maheershakil1224@gmail.com>
2026-03-08 12:50:46 +0500

    Added test data

--- FraudDetection/config.py
+++ FraudDetection/config.py
@@ -1,5 +1,5 @@
 MODEL_PATH = r"C:\Users\HP\Desktop\Recova\FraudDetection\backend\app\model\catboost_model.pkl"
-TEST_DATA_PATH = r"C:\Users\HP\Desktop\Recova\FraudDetection\recova_test_data.xlsx"
+TEST_DATA_PATH = r"C:\Users\HP\Desktop\Recova\FraudDetection\transaction_data.csv"
 
 outfit_bold_path = r"C:\Users\HP\Desktop\Python\Data_Science_Projects\fradulent-transaction-detection\frontend\src\assets\Outfit-Bold.ttf"
 outfit_thin_path = r"C:\Users\HP\Desktop\Python\Data_Science_Projects\fradulent-transaction-detection\frontend\src\assets\Outfit-VariableFont_wght.ttf"
